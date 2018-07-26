78a0bc7f8e4f6be3a765f811e207535c87c00b88
Kishore Relangi <kishore@mobileways.in>
2018-07-26 12:14:48 +0530

    hex preparing to publish

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule PlugIdColumn.MixProject do
       # This option is only needed when you don't want to use the OTP application name
       name: "plug-id-column",
       # These are the default files included in the package
-      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],=
+      files: ["lib", "mix.exs", "README*"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
