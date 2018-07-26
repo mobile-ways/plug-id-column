11353c03a4911377871af69f940fc1f843d67280
Kishore Relangi <kishore@mobileways.in>
2018-07-26 12:27:26 +0530

    hex preparing to publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugIdColumn.MixProject do
   def project do
     [
       app: :plug_id_column,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
