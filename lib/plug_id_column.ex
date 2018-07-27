bffa20e8ac6240e04fc2f0d0b49dad5278063b98
Kishore Relangi <kishore@mobileways.in>
2018-07-27 14:59:16 +0530

    handle data_key

--- lib/plug_id_column.ex
+++ lib/plug_id_column.ex
@@ -10,10 +10,14 @@ defmodule PlugIdColumn do
       conn |> Plug.Conn.register_before_send(&__MODULE__.call_back/1)
   end
 
-  def call_back(%{resp_body: resp_body, params: %{"id_column" => key}}=conn) do
+  def call_back(%{resp_body: resp_body, params: %{"id_column" => key, "data_key" => data_key}}=conn), do: hash_json(conn, resp_body, data_key, key)
+  def call_back(%{resp_body: resp_body, params: %{"id_column" => key}}=conn), do: hash_json(conn, resp_body, nil, key)
+  def call_back(conn), do: conn
+
+  defp hash_json(conn, resp_body, data_key, key) do
     encoder = get_json_encoder()
     with {:ok, %{"data" => data}=body} <- encoder.decode(resp_body),
-      data <- hash_id_column(data, key),
+      data <- hash_id_column(data, data_key, key),
       body <-  Map.merge(body, %{"data" => data}),
       {:ok, body} <- encoder.encode_to_iodata(body) do
         %{conn | resp_body: body}
@@ -22,17 +26,22 @@ defmodule PlugIdColumn do
           conn
     end
   end
-  def call_back(conn), do: conn
 
-  def hash_id_column(data, key) when is_list(data) do
+  defp hash_id_column(data, nil, key) when is_list(data) do
     data |> Enum.map(&({&1 |> Map.get(key), &1})) |> Map.new
   end
-  def hash_id_column(data, _key), do: data
+  defp hash_id_column(data, data_key, key) do
+    hashed = data |> Map.get(data_key) |> hash_id_column(nil, key)
+    Map.put(data, data_key, hashed)
+  end
+  defp hash_id_column(data, _key), do: data
 
   # TUDO: Fix it
   defp get_json_encoder do
     Application.get_env(:phoenix, :format_encoders)
     |> Keyword.get(:json, Poison)
+  rescue(error) ->
+      Poison
   end
 
 end
--- test/plug_id_column_test.exs
+++ test/plug_id_column_test.exs
@@ -2,7 +2,62 @@ defmodule PlugIdColumnTest do
   use ExUnit.Case
   doctest PlugIdColumn
 
-  test "greets the world" do
-    assert PlugIdColumn.hello() == :world
+  test "with data_key and id_column" do
+    payload = %{
+      status: :ok,
+      data: %{
+        some_list: [%{
+          id: 1,
+          name: "one"
+        },%{
+          id: 2,
+          name: "two"
+        },%{
+          id: 3,
+          name: "three"
+        }]
+      }
+    } |> Poison.encode_to_iodata!()
+
+    {data_key, key} = {"some_list", "name"}
+    %{resp_body: resp_body} = PlugIdColumn.call_back(%{resp_body: payload, params: %{"id_column" => key, "data_key" => data_key}})
+    assert %{
+      "data" => %{
+        "some_list" => %{
+          "one" => %{"id" => 1, "name" => "one"},
+          "three" => %{"id" => 3, "name" => "three"},
+          "two" => %{"id" => 2, "name" => "two"}
+        }
+      },
+      "status" => "ok"
+    } = Poison.decode!(resp_body)
+  end
+  
+  test "with only id_column" do
+    payload = %{
+      status: :ok,
+      data: [%{
+        id: 1,
+        name: "one"
+      },%{
+        id: 2,
+        name: "two"
+      },%{
+        id: 3,
+        name: "three"
+      }]
+    } |> Poison.encode_to_iodata!()
+
+    {_data_key, key} = {nil, "name"}
+    %{resp_body: resp_body} = PlugIdColumn.call_back(%{resp_body: payload, params: %{"id_column" => key}})
+
+    assert %{
+      "data" =>  %{
+        "one" => %{"id" => 1, "name" => "one"},
+        "three" => %{"id" => 3, "name" => "three"},
+        "two" => %{"id" => 2, "name" => "two"}
+      },
+      "status" => "ok"
+    } = Poison.decode!(resp_body)
   end
 end
